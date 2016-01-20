67dbef7647421637d22dee01021d8438dbc909d6
Tech <Tech>
2016-01-20 10:47:40 -0500

    Initial Commit

---
+++ Source/Lab1_Console/Lab1_Console.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Lab1_Console.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Lab1_Console, "Lab1_Console" );
---
+++ Source/Lab1_Console/Lab1_ConsoleGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Lab1_ConsoleGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LAB1_CONSOLE_API ALab1_ConsoleGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
